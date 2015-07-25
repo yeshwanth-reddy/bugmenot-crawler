25d336f01438f7def7dc3fab228301edfb193695
Yeshwanth Reddy <yeshwanth.syr@gmail.com>
2015-07-25 15:57:11 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+app.get('/:name?', function(req, res){
+	name = req.params.name;
+	url = 'http://www.bugmenot.com/view/'+name;
+	//console.log(url);
+	request(url, function(error, response, html){
+		if(!error){
+			var $ = cheerio.load(html);
+			var title,temp;
+			var result = [];
+			//console.log($('.account').length);
+			$('.account').each(function() {
+              var data = $(this);
+              var info = data.find('dd kbd');
+              var stat = data.find('.stats li');
+              result.push({
+                username: info.get(0).children[0].data,
+                password: info.get(1).children[0].data,
+                Succesrate: stat.get(0).children[0].data.substring(0, 3),
+                votes: stat.get(1).children[0].data
+              });
+            });
+            res.send(JSON.stringify(result, 0, 4));
+		}
+	});
+});
+
+app.listen('8081')
+console.log('localhost:8081/<site_name>');
+exports = module.exports = app; 	
